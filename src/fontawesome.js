5e40a57fc51353774056a2bfc83c9a321d3f00af
Katheren Salom <katherensalom@gmail.com>
2022-06-30 21:35:35 +0000

    actualizando archivos

--- src/fontawesome.js
+++ src/fontawesome.js
@@ -2,12 +2,6 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 // import your icons
-import { faMoneyBill } from '@fortawesome/pro-solid-svg-icons';
-import { faCode, faHighlighter } from '@fortawesome/free-solid-svg-icons';
+import { faCalendar } from '@fortawesome/free-regular-svg-icons';
 
-library.add(
-  faMoneyBill,
-  faCode,
-  faHighlighter
-  // more icons go here
-);
+library.add(faCalendar);
--- src/js/component/SecondsCountes.jsx
+++ src/js/component/SecondsCountes.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+
+import "../../fontawesome";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function SecondsCounter(props)
 {
     return (<div className="contar">
-        <div className="calendario"><FontAwesomeIcon icon={ faMugSaucer } /></div>
+        <div className="calendario"><FontAwesomeIcon icon="faCalendar" /></div>
         <div className=""></div>
         <div className=""></div>
         <div className=""></div>
