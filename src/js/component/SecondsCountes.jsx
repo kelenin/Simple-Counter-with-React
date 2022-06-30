851c19861864f9912b82afb351876a059ac5ff71
Katheren Salom <katherensalom@gmail.com>
2022-06-30 22:24:49 +0000

    se mejoraron los archivos

--- src/js/component/SecondsCountes.jsx
+++ src/js/component/SecondsCountes.jsx
@@ -1,20 +1,31 @@
 import React from "react";
 
+import PropTypes from "prop-types";
+
 import "../../fontawesome";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function SecondsCounter(props)
 {
     return (<div className="contar">
-        <div className="calendario"><FontAwesomeIcon icon="faCalendar" /></div>
-        <div className="six">0</div>
-        <div className="five">0</div>
-        <div className="four">0</div>
-        <div className="three">0</div>
-        <div className="two">0</div>
-        <div className="one">0</div>
+        <div className="calendario"></div>
+        <div className="six">{props.dig_seis}</div>
+        <div className="five">{props.dig_cinco}</div>
+        <div className="four">{props.dig_cuatro}</div>
+        <div className="three">{props.dig_tres}</div>
+        <div className="two">{props.dig_dos}</div>
+        <div className="one">{props.dig_uno}</div>
 
     </div>);
+}
+
+SecondsCounter.propTypes={
+    dig_uno: PropTypes.number,
+    dig_dos: PropTypes.number,
+    dig_tres: PropTypes.number,
+    dig_cuatro: PropTypes.number,
+    dig_cinco: PropTypes.number,
+    dig_seis: PropTypes.number
 }
 
 export default SecondsCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -9,9 +9,21 @@ import "../styles/index.css";
 import SecondsCounter from "./component/SecondsCountes.jsx";
 
 //render your react application
-const counter=0;
+let counter=0;
 setInterval(function() {
+    const six = Math.floor(counter/100000);
+    const five = Math.floor(counter/10000);
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    counter++;
+
     ReactDOM.render(
-        <SecondsCounter digito={counter} />, document.querySelector("#app")
+        <SecondsCounter dig_uno={one} dig_dos={two} dig_tres={three} dig_cuatro={four} dig_cinco={five} dig_seis={six} />, 
+        document.querySelector("#app")
     );
+   
 }, 1000);
+
+
