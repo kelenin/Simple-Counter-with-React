2ab222905602c49ae7da770e5e329bfb62f2b3ec
Katheren Salom <katherensalom@gmail.com>
2022-06-30 21:17:18 +0000

    actualizando

--- src/js/component/SecondsCountes.jsx
+++ src/js/component/SecondsCountes.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function SecondsCounter(props)
 {
     return (<div className="contar">
-        <div className="calendario"><i class="fa-light fa-clock"></i></div>
+        <div className="calendario"><FontAwesomeIcon icon={ faMugSaucer } /></div>
         <div className=""></div>
         <div className=""></div>
         <div className=""></div>
