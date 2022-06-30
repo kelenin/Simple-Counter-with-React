ef5791776d625e6d4e29e2f73b79cb13c21104be
Katheren Salom <katherensalom@gmail.com>
2022-06-30 20:56:43 +0000

    aqui estoy

--- src/js/component/SecondsCountes.jsx
+++ src/js/component/SecondsCountes.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function SecondsCounter(props)
 {
     return (<div className="contar">
-        <div className="calendario"></div>
+        <div className="calendario"><i class="fa-light fa-clock"></i></div>
         <div className=""></div>
         <div className=""></div>
         <div className=""></div>
