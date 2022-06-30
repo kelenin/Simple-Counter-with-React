d28f0f2767b3b61fa401fda70e67b1d37380c827
Katheren Salom <katherensalom@gmail.com>
2022-06-30 20:48:57 +0000

    se creo archivo y agregando codigo. se modifico el archivo index

---
+++ src/js/component/SecondsCountes.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SecondsCounter(props)
+{
+    return (<div>
+        <div><p>Hello</p></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>);
+}
+
+export default SecondsCounter;
